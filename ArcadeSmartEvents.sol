f31ae498b476cebc7766c0941be1014b84221c1a
shtr86 <79358281+shtr86@users.noreply.github.com>
2023-09-22 12:20:54 +0330

    Update ArcadeSmartEvents.sol

--- ArcadeSmartEvents.sol
+++ ArcadeSmartEvents.sol
@@ -1,18 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.7;
 
-
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-}
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ArcadeSmartEvents {
    
